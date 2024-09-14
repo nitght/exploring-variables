e506ca8f567f9846d5b62c14b5d5210d9212b0d5
Eoin McAndrews <eoinmcandrews@ESA-3.local>
2024-09-14 12:56:14 +0100

    My profile input created

---
+++ age.py
@@ -0,0 +1,6 @@
+name = input("what is your name ")
+age = int(input("what is your age "))
+height = float(input("how tall are you "))
+agein5years = age + 5
+ageinmonths = age * 12
+print("My name is ", name, "I am ", age, "and I am", height, "meters tall. I will be", agein5years, "in 5 years. my age and months", ageinmonths)
